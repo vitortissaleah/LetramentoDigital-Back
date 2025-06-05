f41e02671cf0b8359870fee1730a1e56482f09f2
unknown <tissaleahv@gmail.com>
2025-06-05 13:42:01 -0300

    enviando edição

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use("/auth", authRoutes);
 app.use("/user", userRoutes);
 app.use("/cursos", cursosRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
